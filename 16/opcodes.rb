54369b225213a1405b2e21e7b7d58ddeb21b1c05
Paarth Madan <paarth.madan@shopify.com>
2018-12-25 15:44:52 -0500

    Using some helper functions, manually assigned opcodes numbers

--- 16/opcodes.rb
+++ 16/opcodes.rb
@@ -28,14 +28,18 @@ eqri = create_opcode { |reg, op| reg[op[3]] = (reg[op[1]] == op[2]) ? 1 : 0 }
 eqrr = create_opcode { |reg, op| reg[op[3]] = (reg[op[1]] == reg[op[2]]) ? 1 : 0 }
 
 OPCODES = [addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr]
+OPCODES_MAP = Hash.new { |h, k| h[k] = [] }
 
 def determine_opcode_count(before, operations, after)
   count = 0
 
-  OPCODES.each do |opcode|
+  OPCODES.each.with_index do |opcode, index|
     register = before.dup
     opcode.call(register, operations)
-    count += 1 if register == after
+    if register == after
+      count += 1
+      OPCODES_MAP[index] << operations[0]
+    end
   end
 
   count
@@ -55,3 +59,31 @@ three_count = 0
 end
 
 puts three_count
+
+def print_map
+  OPCODES_MAP.each do |key, value|
+     freq = value.inject(Hash.new(0)) { |h, v| h[v] += 1; h }
+     p "#{key} --> #{freq}"
+   end
+  puts "––––––––––––––––––––––––––––"
+end
+
+def delete(q)
+  OPCODES_MAP.each { |key, value| value.tap {|x| x.delete(q)}}
+  print_map
+end
+
+delete(10)
+delete(13)
+delete(5)
+delete(12)
+delete(6)
+delete(0)
+delete(2)
+delete(3)
+delete(7)
+delete(1)
+delete(9)
+delete(11)
+delete(14)
+delete(8)
