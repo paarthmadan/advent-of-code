37f884a6f4c37e3fad6dee66092152549dc06a1b
Paarth Madan <paarth.madan@shopify.com>
2018-12-25 16:08:25 -0500

    Make Proc more efficient

--- 16/opcodes.rb
+++ 16/opcodes.rb
@@ -1,31 +1,31 @@
 def create_opcode(&process)
   Proc.new do |register, operations|
-    yield(register, operations)
+    register[operations[3]] = yield(register, operations)
   end
 end
 
-addr = create_opcode { |reg, op| reg[op[3]] = reg[op[1]] + reg[op[2]] }
-addi = create_opcode { |reg, op| reg[op[3]] = reg[op[1]] + op[2] }
+addr = create_opcode { |reg, op| reg[op[1]] + reg[op[2]] }
+addi = create_opcode { |reg, op| reg[op[1]] + op[2] }
 
-mulr = create_opcode { |reg, op| reg[op[3]] = reg[op[1]] * reg[op[2]] }
-muli = create_opcode { |reg, op| reg[op[3]] = reg[op[1]] * op[2] }
+mulr = create_opcode { |reg, op| reg[op[1]] * reg[op[2]] }
+muli = create_opcode { |reg, op| reg[op[1]] * op[2] }
 
-banr = create_opcode { |reg, op| reg[op[3]] = reg[op[1]] & reg[op[2]] }
-bani = create_opcode { |reg, op| reg[op[3]] = reg[op[1]] & op[2] }
+banr = create_opcode { |reg, op| reg[op[1]] & reg[op[2]] }
+bani = create_opcode { |reg, op| reg[op[1]] & op[2] }
 
-borr = create_opcode { |reg, op| reg[op[3]] = reg[op[1]] | reg[op[2]] }
-bori = create_opcode { |reg, op| reg[op[3]] = reg[op[1]] | op[2] }
+borr = create_opcode { |reg, op| reg[op[1]] | reg[op[2]] }
+bori = create_opcode { |reg, op| reg[op[1]] | op[2] }
 
-setr = create_opcode { |reg, op| reg[op[3]] = reg[op[1]] }
-seti = create_opcode { |reg, op| reg[op[3]] = op[1] }
+setr = create_opcode { |reg, op| reg[op[1]] }
+seti = create_opcode { |reg, op| op[1] }
 
-gtir = create_opcode { |reg, op| reg[op[3]] = (op[1] > reg[op[2]]) ? 1 : 0 }
-gtri = create_opcode { |reg, op| reg[op[3]] = (reg[op[1]] > op[2]) ? 1 : 0 }
-gtrr = create_opcode { |reg, op| reg[op[3]] = (reg[op[1]] > reg[op[2]]) ? 1 : 0 }
+gtir = create_opcode { |reg, op| (op[1] > reg[op[2]]) ? 1 : 0 }
+gtri = create_opcode { |reg, op| (reg[op[1]] > op[2]) ? 1 : 0 }
+gtrr = create_opcode { |reg, op| (reg[op[1]] > reg[op[2]]) ? 1 : 0 }
 
-eqir = create_opcode { |reg, op| reg[op[3]] = (op[1] == reg[op[2]]) ? 1 : 0 }
-eqri = create_opcode { |reg, op| reg[op[3]] = (reg[op[1]] == op[2]) ? 1 : 0 }
-eqrr = create_opcode { |reg, op| reg[op[3]] = (reg[op[1]] == reg[op[2]]) ? 1 : 0 }
+eqir = create_opcode { |reg, op| (op[1] == reg[op[2]]) ? 1 : 0 }
+eqri = create_opcode { |reg, op| (reg[op[1]] == op[2]) ? 1 : 0 }
+eqrr = create_opcode { |reg, op| (reg[op[1]] == reg[op[2]]) ? 1 : 0 }
 
 OPCODES = [addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr]
 OPCODES_MAP = Hash.new { |h, k| h[k] = [] }
