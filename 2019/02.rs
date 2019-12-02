7f45ca7b4ec407945bcad45205f079a1c96c4ec1
Paarth Madan <paarth.madan@shopify.com>
2019-12-02 08:33:23 -0500

    (2019): day 2, part 2

--- 2019/02.rs
+++ 2019/02.rs
@@ -3,7 +3,6 @@ mod helper;
 use helper::exit;
 use std::env;
 
-
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
@@ -11,17 +10,30 @@ fn main() {
     };
 
     let file = &helper::load_file(&args[1])[0];
-    let mut input: Vec<u32> = file
-        .split(",")
-        .map(|x| x.parse::<u32>().unwrap())
-        .collect();
 
-    eval(&mut input, 0);
-    println!("{}", input[0]);
+    let input: Vec<u32> = file.split(",").map(|x| x.parse::<u32>().unwrap()).collect();
+
+    for noun in 0..=99 {
+        for verb in 0..=99 {
+            let mut copy = input.to_owned();
+
+            copy[1] = noun;
+            copy[2] = verb;
+
+            eval(&mut copy, 0);
+
+            if copy[0] == 19690720 {
+                println!("Noun: {}, Verb: {}", noun, verb);
+                println!("100 x noun + verb = {}", 100 * noun + verb);
+            }
+        }
+    }
 }
 
 fn eval(input: &mut Vec<u32>, pointer: usize) {
-    if (pointer >= input.len() - 1) { return; }
+    if pointer >= input.len() - 1 {
+        return;
+    }
     let op = input[pointer];
     match op {
         1 => compute(input, pointer, true),
