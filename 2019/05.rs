e769fd7a900a682e181903ff155a5d2c49d25a0a
Paarth Madan <paarth.madan@shopify.com>
2019-12-07 10:58:32 -0500

    (2019): Clean up intcode, day 5

--- 2019/05.rs
+++ 2019/05.rs
@@ -1,116 +1,112 @@
 mod helper;
 
 use helper::exit;
+use std::cell::RefCell;
 use std::{env, io};
+use Instr2::*;
+use Instr3::*;
+use Instr4::*;
+use InstructionType::*;
+use Status::*;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        exit("Usage: 05 <file>");
-    };
-
-    let file = &helper::load_file(&args[1])[0];
-
-    let mut input: Vec<i32> = file.split(",").map(|x| x.parse::<i32>().unwrap()).collect();
-    perform(&mut input);
+struct Instruction<'a> {
+    instr_type: InstructionType,
+    input: &'a RefCell<Vec<i32>>,
+    pc: &'a RefCell<usize>,
 }
 
-fn perform(mut input: &mut Vec<i32>) {
-    const TERMINATE: i32 = 99;
+enum InstructionType {
+    Instr2(Instr2),
+    Instr3(Instr3),
+    Instr4(Instr4),
+    Terminate,
+}
 
-    let mut pc: usize = 0;
-    let mut opcode;
+enum Instr2 {
+    InputAndSave,
+    Output,
+}
 
-    loop {
-        opcode = parse_opcode(&input[pc]);
-        match opcode {
-            1 | 2 | 7 | 8 => instr4(opcode, &mut input, &mut pc),
-            3 | 4 => instr2(opcode, &mut input, &mut pc),
-            5 | 6 => instr3(opcode, &mut input, &mut pc),
-            99 => break,
-            _ => unreachable! {},
+enum Instr3 {
+    JumpIfTrue,
+    JumpIfFalse,
+}
+
+enum Instr4 {
+    Addition,
+    Multiplication,
+    LessThan,
+    Equal,
+}
+
+enum Status {
+    Continue,
+    Halt,
+}
+
+impl Instruction<'_> {
+    fn new<'a>(
+        instr: InstructionType,
+        input: &'a RefCell<Vec<i32>>,
+        pc: &'a RefCell<usize>,
+    ) -> Instruction<'a> {
+        Instruction {
+            instr_type: instr,
+            input: input,
+            pc: pc,
         }
     }
-}
 
-fn instr4(opcode: i32, instr: &mut Vec<i32>, ptr: &mut usize) {
-    let modes = parse_parameter_modes(&instr[*ptr as usize], 2);
-
-    let s1 = resolve(instr, &instr[*ptr + 1], modes[0]);
-    let s2 = resolve(instr, &instr[*ptr + 2], modes[1]);
-    let dst = instr[*ptr + 3] as usize;
-
-    match opcode {
-        1 => instr[dst] = s1 + s2,
-        2 => instr[dst] = s1 * s2,
-        7 => {
-            if s1 < s2 {
-                instr[dst] = 1;
-            } else {
-                instr[dst] = 0;
+    fn call(&self) -> Status {
+        let mut input = self.input.borrow_mut();
+        let mut ptr = self.pc.borrow_mut();
+        match &self.instr_type {
+            Instr2(op) => {
+                let modes = parse_parameter_modes(&input[*ptr as usize], 1);
+                let p1 = input[*ptr + 1];
+                match op {
+                    InputAndSave => {
+                        let mut num = String::new();
+                        io::stdin()
+                            .read_line(&mut num)
+                            .expect("Failed to read from STDIN");
+                        let num = num.trim().parse::<i32>().unwrap();
+                        input[p1 as usize] = num;
+                    }
+                    Output => println!("{}", resolve(&input, &p1, modes[0])),
+                }
+                *ptr += 2;
             }
+            Instr3(op) => {
+                let modes = parse_parameter_modes(&input[*ptr as usize], 2);
+                let s1 = resolve(&input, &input[*ptr + 1], modes[0]);
+                let s2 = resolve(&input, &input[*ptr + 2], modes[1]);
+                match op {
+                    JumpIfTrue => *ptr = if s1 != 0 { s2 as usize } else { *ptr + 3 },
+                    JumpIfFalse => *ptr = if s1 == 0 { s2 as usize } else { *ptr + 3 },
+                }
+            }
+            Instr4(op) => {
+                let modes = parse_parameter_modes(&input[*ptr as usize], 2);
+                let s1 = resolve(&input, &input[*ptr + 1], modes[0]);
+                let s2 = resolve(&input, &input[*ptr + 2], modes[1]);
+                let dst = input[*ptr + 3] as usize;
+
+                match op {
+                    Addition => input[dst] = s1 + s2,
+                    Multiplication => input[dst] = s1 * s2,
+                    LessThan => input[dst] = (s1 < s2) as i32,
+                    Equal => input[dst] = (s1 == s2) as i32,
+                }
+                *ptr += 4;
+            }
+            Terminate => return Status::Halt,
         }
-        8 => {
-            if s1 == s2 {
-                instr[dst] = 1;
-            } else {
-                instr[dst] = 0;
-            }
-        }
-        _ => unreachable! {},
+        Status::Continue
     }
-
-    *ptr += 4;
-}
-
-fn instr2(opcode: i32, instr: &mut Vec<i32>, ptr: &mut usize) {
-    let modes = parse_parameter_modes(&instr[*ptr as usize], 1);
-    let p1 = instr[*ptr + 1];
-    match opcode {
-        3 => {
-            let mut input = String::new();
-            io::stdin()
-                .read_line(&mut input)
-                .expect("Failed to read from STDIN");
-            let num = input.trim().parse::<i32>().unwrap();
-            instr[p1 as usize] = num;
-        }
-        4 => println!("{}", resolve(instr, &p1, modes[0])),
-        _ => unreachable! {},
-    }
-    *ptr += 2;
-}
-
-fn instr3(opcode: i32, instr: &mut Vec<i32>, ptr: &mut usize) {
-    let modes = parse_parameter_modes(&instr[*ptr as usize], 2);
-    let s1 = resolve(instr, &instr[*ptr + 1], modes[0]);
-    let s2 = resolve(instr, &instr[*ptr + 2], modes[1]);
-
-    match opcode {
-        5 => {
-            if s1 != 0 { 
-                *ptr = s2 as usize; 
-            } else {
-                *ptr += 3;
-            }
-        },
-        6 => {
-            if s1 == 0 { 
-                *ptr = s2 as usize; 
-            } else {
-                *ptr += 3;
-            }
-        },
-        _ => unreachable! {},
-    }
-}
-
-fn parse_opcode(instr1: &i32) -> i32 {
-    *instr1 % 100
 }
 
 fn parse_parameter_modes(instr1: &i32, min: u8) -> Vec<u8> {
-    // Remove last 2 digits (opcode)
     let mut v = *instr1 / 100;
     let mut modes: Vec<u8> = Vec::new();
 
@@ -128,4 +124,47 @@ fn resolve(instr: &Vec<i32>, p: &i32, mode: u8) -> i32 {
         1 => *p,
         _ => unreachable! {},
     }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        exit("Usage: 05 <file>");
+    };
+
+    let file = &helper::load_file(&args[1])[0];
+
+    let input: Vec<i32> = file.split(",").map(|x| x.parse::<i32>().unwrap()).collect();
+    perform(input);
+}
+
+fn perform(input: Vec<i32>) {
+    let input = RefCell::new(input);
+    let pc: RefCell<usize> = RefCell::new(0);
+    let mut opcode;
+
+    loop {
+        opcode = parse_opcode(&input.borrow()[*pc.borrow()]);
+        let instr = match opcode {
+            1 => Instruction::new(Instr4(Addition), &input, &pc),
+            2 => Instruction::new(Instr4(Multiplication), &input, &pc),
+            3 => Instruction::new(Instr2(InputAndSave), &input, &pc),
+            4 => Instruction::new(Instr2(Output), &input, &pc),
+            5 => Instruction::new(Instr3(JumpIfTrue), &input, &pc),
+            6 => Instruction::new(Instr3(JumpIfFalse), &input, &pc),
+            7 => Instruction::new(Instr4(LessThan), &input, &pc),
+            8 => Instruction::new(Instr4(Equal), &input, &pc),
+            99 => Instruction::new(Terminate, &input, &pc),
+            _ => unreachable! {},
+        };
+
+        match instr.call() {
+            Continue => continue,
+            Halt => break,
+        }
+    }
+}
+
+fn parse_opcode(instr1: &i32) -> i32 {
+    *instr1 % 100
 }
