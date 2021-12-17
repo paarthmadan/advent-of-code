c9eec5ab08aace7859e4b5fcfcf167fa2e5dada6
Paarth Madan <paarth.mdn@gmail.com>
2021-12-17 14:48:23 -0500

    (2021): day 4, pt2

--- 2021/04.rs
+++ 2021/04.rs
@@ -8,12 +8,14 @@ struct Board {
     board_state_encoded: u32,
     board_map: HashMap<u32, (usize, usize)>,
     length: usize,
+    winning_draw: Option<u32>,
 }
 
 impl Board {
     pub fn from_matrix(matrix: Vec<Vec<u32>>) -> Self {
         let mut board_map: HashMap<u32, (usize, usize)> = HashMap::new();
         let board_state_encoded: u32 = 0;
+        let winning_draw = None;
         let length = matrix.len();
 
         for (y, row) in matrix.into_iter().enumerate() {
@@ -26,14 +28,19 @@ impl Board {
             board_map,
             board_state_encoded,
             length,
+            winning_draw,
         }
     }
 
-    pub fn update(&mut self, number: u32) {
+    fn update(&mut self, number: u32) {
         match self.board_map.get(&number).copied() {
             Some(pos) => self.encode(pos),
             None => {}
         }
+    }
+
+    fn set_winning_draw(&mut self, draw: u32) {
+        self.winning_draw = Some(draw);
     }
 
     fn encode(&mut self, pos: (usize, usize)) {
@@ -143,16 +150,28 @@ fn main() {
     let input: Vec<String> = helper::load_file(&args[1]);
     let draws = parse_draws(&input[0]);
     let mut boards = parse_boards(input);
+    let mut completed_boards: Vec<Board> = Vec::with_capacity(boards.len());
 
     'draw: for draw in draws {
         for board in &mut boards {
             board.update(draw);
         }
-        for board in &boards {
-            if board.check_board() {
-                println!("{}", board.score(draw));
-                break 'draw;
-            }
+        let (mut completed, incompleted): (Vec<Board>, Vec<Board>) =
+            boards.into_iter().partition(|b| b.check_board());
+
+        boards = incompleted;
+        completed.iter_mut().for_each(|b| b.set_winning_draw(draw));
+        completed_boards.append(&mut completed);
+
+        if boards.len() == 0 {
+            break 'draw;
         }
     }
+
+    let score = match completed_boards.pop() {
+        Some(board) => board.score(board.winning_draw.unwrap()),
+        None => 0,
+    };
+
+    println!("{}", score);
 }
